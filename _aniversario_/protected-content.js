71d659768fe2b91f23321181b3b889b198551595
Patrick <patrick.americo@outlook.com>
2025-04-28 16:24:22 -0300

    aniversario

---
+++ _aniversario_/protected-content.js
@@ -0,0 +1,36 @@
+export default {
+  template: `
+    <main class="container" role="main">
+      <h1>Feliz Aniversário</h1>
+
+      <h3>Feliz Aniversário, Meu Amor!</h3>
+      <p>
+        Hoje é um dia muito especial. Há anos você fala da passagem das três
+        décadas de vida, e são três décadas das quais só na infância eu não
+        tive a oportunidade de ver você, mas tive o grande prazer de entrar na
+        sua adolescência e ver você se tornar a mulher mais linda e incrível
+        que eu conheço.
+      </p>
+
+      <h3>Você é o Centro do Meu Mundo</h3>
+      <p>
+        Desde que nos conhecemos, você virou o centro do meu mundo. Seu sorriso
+        radiante, sua alegria contagiante, seu jeito único de ser, tudo em você
+        me conquista a cada dia. Você é a pessoa mais especial da minha vida e
+        espero que aproveite esse dia tão importante com muita alegria, saúde
+        e amor.
+      </p>
+
+      <h3>Que Deus te Abençoe Sempre!</h3>
+      <p>
+        Feliz aniversário, minha vida, com todo o meu coração. Que Deus te
+        abençoe sempre e continue iluminando seu caminho com sua graça e
+        misericórdia. Você merece todo o amor e felicidade deste mundo.
+      </p>
+
+      <div class="age-container" aria-hidden="true">
+        <div class="age">30</div>
+      </div>
+    </main>
+  `,
+};
